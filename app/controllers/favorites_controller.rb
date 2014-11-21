04e7d9b68acf012d1d479e1e9696ae48daca330e
Stephanie-Chou <stephanie.chou@outlook.com>
2014-11-20 20:40:44 -0800

    favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,45 @@
 class FavoritesController < ApplicationController
+  respond_to :json
+  before_filter :find_user
+
+  def index
+    @favorites = Favorite.all
+    respond_with @favorites
+  end
+
+  def create
+    @favorite = Favorite.create favorite_params
+    respond_with @favorite
+  end
+
+  def show
+    respond_with @favorite
+  end
+
+  def update
+    @favorite.update_attributes favorite_params
+    respond_with @favorite
+  end
+
+  def destroy
+    @favorite.destroy
+    head 204
+  end
+
+  protected
+    def find_favorite
+      @favorite = Favorite.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      head 404
+    end
+
+    def find_user
+      @user = User.find params[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      head 404
+    end
+
+    def favorite_params
+      params.require(:favorite).permit(:user_id, :cut_id)
+    end
 end
--- db/migrate/20141121014115_create_favorites.rb
+++ db/migrate/20141121014115_create_favorites.rb
@@ -2,7 +2,7 @@ class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
       t.integer :cut_id
-      t.user :user_id
+      t.integer :user_id
 
       t.timestamps
     end
